191a120b51b7dcf2ec1e7ad629ad266df86609ea
chenyw <chen_yw@126.com>
2018-05-28 18:03:29 +0800

    update userinfo

--- app/Laravel/app/Http/Controllers/UsersController.php
+++ app/Laravel/app/Http/Controllers/UsersController.php
@@ -3,12 +3,68 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UsersController extends Controller
 {
-    
-    public function create()
+	// 注册页
+	public function create()
     {
         return view('users.create');
     }
+
+    // 编辑页
+    public function edit(User $user)
+    {
+    	return view('users.edit', compact('user'));
+    }
+
+    // 展示页
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    // 注册入库
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        Auth::login($user);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show', [$user]);
+    }
+
+    // 编辑入库
+    public function update(User $user, Request $request)
+    {
+    	$this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+
+        $data = [];
+        $data['name'] = $request->name;
+        if ( $request->password ) {
+        	$data['password'] = bcrypt($request->password);
+        }
+        $user->update($data);
+
+        session()->flash('success', '个人资料更新成功！');
+
+        return redirect()->route('users.show', $user->id);
+    }
+
+
 }
